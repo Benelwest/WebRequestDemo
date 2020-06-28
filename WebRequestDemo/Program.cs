4bd5c51e5b079438b064fdc195d1430bb2304946
ben <benell@bluewin.ch>
2020-06-28 22:09:07 +0200

    Projektdateien hinzufügen.

---
+++ WebRequestDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebRequestDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = HttpClientFactory.Create();
+
+            var url = "https://jsonplaceholder.typicode.com/todos/1";
+            //var data = await httpClient.GetStringAsync(url);
+            
+           HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+
+            if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+            {
+                var content = httpResponseMessage.Content;
+                var data = await content.ReadAsStringAsync();
+                Console.WriteLine(data);
+
+            }
+         
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
